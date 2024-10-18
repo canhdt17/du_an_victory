45e83183341971bfecc0c812d0420040ebb14d2a
canhdt17 <tadongcanh2k4@gmail.com>
2024-10-18 12:14:12 +0700

    sua bang showtime

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::resource('showtimes', ShowtimeController::class);
 Route::prefix('admin')->group(function () {
     Route::resource('showtime', ShowtimeController::class);
     Route::get('/', function () {
