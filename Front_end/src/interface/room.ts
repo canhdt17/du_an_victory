0571ee4c0a119b0ca294107d0491ddccd1061716
canhdt17 <tadongcanh2k4@gmail.com>
2024-11-18 20:51:56 +0700

    Delete Front_end directory

--- Front_end/src/interface/room.ts
+++
@@ -1,7 +0,0 @@
-export interface IRoom {
-  id?: number | string;
-  room_name: string;
-  area_id: number | string;
-  total_seat: number;
-}
-export type RoomData = Pick<IRoom, "room_name" | "area_id" | "total_seat">;
--- Front_end/src/interface/shotime.ts
+++
@@ -1,8 +0,0 @@
-export interface IShowTime{
-    id:number|string,
-    movie_id:number|string,
-    room_id:number|string,
-    showtime_date:string,
-    start_time:string,
-    end_time:string
-}
