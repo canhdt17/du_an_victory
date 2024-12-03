3c9e8822d25f4789ebe12ce8a7ccc215f2c2d8ed
canhdt17 <tadongcanh2k4@gmail.com>
2024-12-03 22:04:38 +0700

    update

--- Back_end/vendor/composer/installed.php
+++ Back_end/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'abdb261dc17e48a94f7cf3fea8cf9375cabbbe28',
+        'reference' => 'd3042116455418b7f9c48e755cf558ba15a46c79',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'abdb261dc17e48a94f7cf3fea8cf9375cabbbe28',
+            'reference' => 'd3042116455418b7f9c48e755cf558ba15a46c79',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -757,8 +757,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
